eb9a7c2bb5f06af2eec15852c3d002d350a5bb13
except134 <except134@gmail.com>
2021-09-22 08:37:30 +0300

    Task3 in process

--- Lesson2/Task3.h
+++ Lesson2/Task3.h
@@ -1,3 +1,19 @@
 #pragma once
 
-
+enum class CardType : int
+{
+    Zero,
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Vallet,
+    Dama,
+    King
+};
